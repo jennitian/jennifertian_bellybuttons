f9fdcf8f38ea72e8818635281a11a36fd649ee4e
Jennifer Tian <jennifertian@Jennifers-MacBook-Pro.local>
2019-12-29 14:44:09 -0700

    bubble chart

--- plots.js
+++ plots.js
@@ -42,8 +42,46 @@ function buildCharts(sample) {
         var bactArray = bactSamples.filter(sampleObj => sampleObj.id == sample);
         var result = bactArray[0];
         var otuID = result.otu_ids;
-        var topTen = otuID.slice(0,10);
-        console.log(topTen);
+        var sampleValues = result.sample_values;
+        var topOtuIDs = otuID.slice(0,10);
+        var topSampleValues = sampleValues.slice(0,10);
+        console.log(topSampleValues);
+        /*otuIDlabels = topOtuIDs.forEach(label => 
+            label = ('OTU: ' + label));
+        console.log(otuIDlabels)*/
+        // test otuid labels using console log?
+        var trace1 = {
+            x: topSampleValues,
+            y: topOtuIDs,
+            type: 'bar',
+            orientation: 'h'
+        };
+        var layout1 = {
+            title: 'Top Bacterial Species Found',
+            xaxis: {title: 'Sample Value'},
+            yaxis: {
+                title: 'OTU ID',
+                type: 'category',
+                autorange: 'reversed'}
+        };
+        var trace2 = {
+            x: topOtuIDs,
+            y: topSampleValues,
+            mode: 'markers',
+            marker: {
+                size: sampleValues,
+                color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)', 'rgb(255, 65, 54)', 'rgb(255, 65, 54)', 'rgb(255, 65, 54)', 'rgb(255, 65, 54)', 'rgb(255, 65, 54)', 'rgb(255, 65, 54)'],},
+            text: topOtuIDs
+        }
+        var layout2 = {
+            title: 'Bubble Chart of Top Colonies',
+            xaxis: {title: 'OTU ID'},
+            yaxis: {title: 'Sample Value'}
+        }
+
+        Plotly.newPlot("bar", [trace1], layout1);
+
+        Plotly.newPlot("bubble", [trace2], layout2)
 
     });
 }
